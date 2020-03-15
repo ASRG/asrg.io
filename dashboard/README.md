771d10a8669d3a3919ce57852d93184ccce0a470
Christian Engel <engel.christian1994@gmail.com>
2020-03-15 22:19:46 +0100

    initial commit

---
+++ dashboard/README.md
@@ -0,0 +1,8 @@
+# ASRG Dashboard
+
+This is a work in progress. It is inspired by the [pyladies Dashboard](https://nyc-pyladies.github.io/pyladies-dashboard/pyladies.html). The current draft simply includes some widgets and a map.
+
+## TODOs
+
+- [ ] set up static JSON or API interface for meetup data
+- [ ] more components
---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Open Sans", "sans-serif"],
+        display: ["Open Sans", "sans-serif"]
+      },
+      colors: {
+        blueGray: "#4a5568",
+        darkBlue1: "#0e2439",
+        darkBlue2: "#1f364d"
+      },
+      boxShadow: {
+        dark:
+          " 0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
