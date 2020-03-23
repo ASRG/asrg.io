4c91fab2d8dc8b316f008d14002e285dfd9abae6
Christian Engel <engel.christian1994@gmail.com>
2020-03-23 12:47:43 +0100

    Update dashboard config

--- dashboard/postcss.config.js
+++ dashboard/postcss.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   plugins: [require("tailwindcss"), require("autoprefixer")]
 };
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.js", "./src/**/*.jsx", "./public/index.html"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [purgecss])
+  ]
+};
+
