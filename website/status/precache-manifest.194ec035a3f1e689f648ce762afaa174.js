5e4fd7480d90b5432a053eec22dcca8aef1e1e8e
Christian Engel <engel.christian1994@gmail.com>
2020-03-23 12:47:28 +0100

    Build dashboard into status subdir

---
+++ website/status/precache-manifest.194ec035a3f1e689f648ce762afaa174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994a29a11a968a91d347dcd02bf59093",
+    "url": "/status/index.html"
+  },
+  {
+    "revision": "543f6fb842beeb6e1bc5",
+    "url": "/status/static/css/main.20acb61e.chunk.css"
+  },
+  {
+    "revision": "6b495c6138c506446c7e",
+    "url": "/status/static/js/2.b143aee8.chunk.js"
+  },
+  {
+    "revision": "737c637c7902752c5b98905ed1dd3b22",
+    "url": "/status/static/js/2.b143aee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543f6fb842beeb6e1bc5",
+    "url": "/status/static/js/main.437b2a8a.chunk.js"
+  },
+  {
+    "revision": "fe21b25eca3acf0cbc59",
+    "url": "/status/static/js/runtime-main.43b61323.js"
+  },
+  {
+    "revision": "0758dab5c5373d80fff85dd7cbef5ce5",
+    "url": "/status/static/media/logo.0758dab5.png"
+  }
+]);
