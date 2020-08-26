2b5a871980263337a9cdf76aecf86315be683cc2
Brandon Barry <brandonjkbarry@gmail.com>
2020-08-26 11:37:39 -0400

    changed database initializing script to support new app container name

--- member-portal/django-dashboard-coreui/dev/recreate_database.sh
+++ member-portal/django-dashboard-coreui/dev/recreate_database.sh
@@ -3,7 +3,7 @@
 DB_CONTAINER="${1:-django-dashboard-coreui_postgres_1}"
 USERNAME="${2:-changeme}"
 DATABASE="${3:-asrg}"
-APP_CONTAINER="${4:-django-dashboard-coreui_appseed-app_1}"
+APP_CONTAINER="${4:-asrg-app}"
 
 docker exec ${DB_CONTAINER} psql -U ${USERNAME} -d postgres -c "DROP DATABASE ${DATABASE};"
 docker exec ${DB_CONTAINER} psql -U ${USERNAME} -d postgres -c "CREATE DATABASE ${DATABASE};"
