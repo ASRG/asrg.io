d54d375924128b14d87aaa1bddacc7029995dd0e
John Heldreth <john.heldreth@porsche-engineering.de>
2020-09-20 18:02:19 +0200

    Updated views.py with event render

--- member-portal/django-dashboard-coreui/website/views.py
+++ member-portal/django-dashboard-coreui/website/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
 
 def landing(request):
     return render(request, "landing.html")
 
+
 def events(request):
-    return HttpResponse ('<p>Events View</p>')
-    #return render(request, "events.html")
+    events = event.objects.all{}
+    return render(request, 'events.html', {
+        'events': events,
+    })
+
 
 def event_details(request, event_id):
-    return HttpResponse (f'<p>Event Detail View = {event_id}</p>')
-    #return render(request, "events.html")
+    try:
+        event = events.objects.get(id=event_id)
+    except event.DoesNotExist:
+        raise Http404('Event does not exist')
+    return render(request, 'event_detail.html', {
+        'event': event,
+    })
