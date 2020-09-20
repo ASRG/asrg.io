6490396cb162c1d45c042f7821ecf8676dd12483
John Heldreth <john.heldreth@porsche-engineering.de>
2020-09-20 17:46:55 +0200

    Added Event and Event Detail to views.py

--- member-portal/django-dashboard-coreui/website/views.py
+++ member-portal/django-dashboard-coreui/website/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def landing(request):
     return render(request, "landing.html")
+
+def events(request):
+    return HttpResponse {'Events View'}
+    #return render(request, "events.html")
+
+def event_details(request, event_id):
+    return HttpResponse {'Event Detail View = {event_id}'}
+    #return render(request, "events.html")
