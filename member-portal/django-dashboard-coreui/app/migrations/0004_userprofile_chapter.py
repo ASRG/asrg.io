70c7964bc194e40387ca7f76b99b925dbde32667
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2020-09-22 19:41:06 +0300

    commit changes

--- member-portal/django-dashboard-coreui/app/migrations/0004_userprofile_chapter.py
+++ member-portal/django-dashboard-coreui/app/migrations/0004_userprofile_chapter.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authentication', '0002_auto_20200823_1514'),
+        ('authentication', '0003_auto_20200910_1601'),
         ('app', '0003_remove_userprofile_chapter'),
     ]
 
