8339c26768ab78caf4c4880d11f96a83fe39f526
Sruthy <56227030+sru-thy@users.noreply.github.com>
2020-09-25 02:21:44 +0530

    added images for /contributors

--- member-portal/django-dashboard-coreui/website/models.py
+++ member-portal/django-dashboard-coreui/website/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 
 class JobPosting(models.Model):
@@ -10,7 +12,10 @@ class JobPosting(models.Model):
 
 class Contributor(models.Model):
     name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
     linkedin = models.URLField(max_length=200)
     github = models.URLField(max_length=200)
+    img = ProcessedImageField(upload_to="contributors",verbose_name='Image',blank=False)
+    image_thumbnail = ImageSpecField(source='img', processors=[ResizeToFill(120, 120)], format='JPEG')
     def __str__(self):
         return self.name
