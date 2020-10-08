3d683c0dcf47365906e7f17d871e0b3b395cadcb
Melvin Thomas <melvinchooranolil@gmail.com>
2020-10-08 21:58:00 +0530

    merged /createprofile and /edit-profile

--- member-portal/django-dashboard-coreui/authentication/urls.py
+++ member-portal/django-dashboard-coreui/authentication/urls.py
@@ -7,9 +7,15 @@ Copyright (c) 2019 - present AppSeed.us
 from django.urls import path
 from .views import login_view, register_user
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+
+from core.settings import MEDIA_ROOT, MEDIA_URL, DEBUG
 
 urlpatterns = [
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout")
-]
+] 
+if DEBUG:
+        urlpatterns += static(MEDIA_URL,
+                              document_root=MEDIA_ROOT)
