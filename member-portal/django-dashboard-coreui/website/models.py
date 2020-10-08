f84250eb66bded0468fce19c47ebb8118ffeae3a
peterbogdan <pitcata@gmail.com>
2020-10-08 20:38:10 +0300

    add events nr and links to job postings

--- member-portal/django-dashboard-coreui/website/models.py
+++ member-portal/django-dashboard-coreui/website/models.py
@@ -9,6 +9,8 @@ class JobPosting(models.Model):
     location = models.CharField(max_length=56, null=False, blank=False)
     date_posted = models.DateTimeField(blank=True, auto_now_add=True)
     job_description = models.TextField(blank=True)
+    job_link = models.URLField(blank=True, verbose_name="Job Posting Link")
+    company_name = models.CharField(max_length=80)
 
 class Contributor(models.Model):
     name = models.CharField(max_length=200)
