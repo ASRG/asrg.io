59254171730b5c5714a1bb63670ec929492213b0
alanamircruz <amir.cruz@gmail.com>
2020-10-10 15:22:17 +0200

    ThreatQ dummy page as comming soon, HTML console errors and sources not found fixed, first approach for CSP embedded ThreatQ WIP, updated additional README

--- member-portal/django-dashboard-coreui/website/views.py
+++ member-portal/django-dashboard-coreui/website/views.py
@@ -3,12 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import redirect, get_object_or_404
 
-
 from .models import JobPosting, Contributor, Announcement
 from authentication.models import Chapter, User
 from events.models import Event
 from website.filters import JobPostingFilters
-
 
 def landing(request):
     context = {
@@ -19,52 +17,47 @@ def landing(request):
     }
     return render(request, "landing.html", context=context)
 
-
 def about(request):
     return render(request, "about.html")
-
 
 def blog(request):
     return render(request, "blog.html")
 
-
 def privacy(request):
     return render(request, "privacy.html")
-
 
 def security(request):
     return render(request, "security.html")
 
 
 def register(request):
-    response = redirect('/register/')
-    return response
+    return redirect('/register/')
 
+def threatq(request):
+    return render(request, "threatq.html")
 
 @login_required(login_url="/login/")
 def job_posting(request):
     queryset = JobPosting.objects.all().order_by('-date_posted')
     job_postings = JobPostingFilters(request.GET, queryset=queryset)
     return render(
-        request,
-        "job_posting.html",
-        {
-            'job_postings': job_postings,
-        },
-    )
-
+              request,
+              "job_posting.html",
+              {
+                  'job_postings': job_postings,
+              },
+          )
 
 @login_required(login_url="/login/")
 def job_details(request, job_id):
     job = get_object_or_404(JobPosting, id=job_id)
     return render(
-        request,
-        "job_details.html",
-        {
-            'job': job,
-        },
-    )
-
+             request,
+             "job_details.html",
+             {
+                 'job': job,
+             },
+         )
 
 def contributors(request):
     contributors = Contributor.objects.all()
@@ -72,5 +65,4 @@ def contributors(request):
     return render(request, 'contributors.html', context)
 
 def dashboard(request):
-    response = redirect('index.html')
-    return response
+    return redirect('index.html')
