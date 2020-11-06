55658533d947d8860e1bc8cbf61a845517815073
Melvin Thomas <melvinchooranolil@gmail.com>
2020-11-06 16:37:25 +0530

    added technical committees

--- member-portal/django-dashboard-coreui/django_app/technical_committees/models.py
+++ member-portal/django-dashboard-coreui/django_app/technical_committees/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+VIEW_STATUS_CHOICES = (
+    ('Public', 'Public'),
+    ('Private', 'Private'),
+)
+
+TC_STATUS_CHOICES = (
+    ('Active', 'Active'),
+    ('Inactive', 'Inactive'),
+)
+
+class Technical_committee(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    description = models.TextField(blank=False)
+    short_name = models.CharField(max_length=6, blank=False)
+    view_status = models.CharField(max_length= 20, choices=VIEW_STATUS_CHOICES)
+    tc_status = models.CharField(max_length=15, blank=False)
+    picture = models.ImageField(upload_to='technical-committee', blank=True)
