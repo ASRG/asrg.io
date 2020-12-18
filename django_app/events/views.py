99a461dccb3416cc793dfa84db9c560dda69b435
Melvin Thomas <melvinchooranolil@gmail.com>
2020-12-18 09:16:25 +0530

    display only confirmed events

--- django_app/events/views.py
+++ django_app/events/views.py
@@ -7,14 +7,14 @@ def events_view(request):
     context = {}
     now = timezone.now()
     # events = Event.objects.all()
-    upcoming_events = Event.objects.filter(start_date__gte=now).order_by('start_date')
+    upcoming_events = Event.objects.filter(start_date__gte=now, status=2).order_by('start_date')
     context['upcoming_events'] = upcoming_events
     try:
         next_event = upcoming_events[0]
         context['next_event'] = next_event
     except IndexError:
         pass
-    passed_events = Event.objects.filter(start_date__lt=now).order_by('-start_date')
+    passed_events = Event.objects.filter(start_date__lt=now, status=2).order_by('-start_date')
     context['passed_events'] = passed_events
     if request.user.is_authenticated:
         return render(request, "events_authenticated_users.html",context)
