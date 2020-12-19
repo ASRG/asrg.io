18268688e8363f32bf6cf0191797acac60992c37
Melvin Thomas <melvinchooranolil@gmail.com>
2020-12-19 13:55:20 +0530

    locations page

--- django_app/locations/views.py
+++ django_app/locations/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 from .models import Location
+from authentication.models import Chapter
 
 def location_view(request):
     context = {}
-    locations = Location.objects.all()
+    locations = Chapter.objects.all()
     context['locations'] = locations
     if request.user.is_authenticated:
         return render(request, "locations/locations_authenticated.html",context)
