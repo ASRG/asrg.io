af97f235a07eeb606b577d95fc966ee902c111da
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-02-07 23:29:48 +0200

    use single template instead of 2 templates for authenticated/not

--- django_app/events/views.py
+++ django_app/events/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Event
+
 
 def events_view(request):
     context = {}
@@ -17,19 +18,17 @@ def events_view(request):
     passed_events = Event.objects.filter(start_date__lt=now, status=2).order_by('-start_date')
     context['passed_events'] = passed_events
     if request.user.is_authenticated:
-        return render(request, "events/events_authenticated_users.html",context)
+        context['base_template'] = "layouts/base.html"
     else:
-        return render(request, "events/events.html",context)
-
+        context['base_template'] = "landing_page/base.html"
+    return render(request, "events/events.html", context)
 
 
 def event_detail_view(request, event_id):
     event = get_object_or_404(Event, id=event_id)
+    context = {"event": event}
     if request.user.is_authenticated:
-        return render (request, "events/event_detail_authenticated_users.html", {
-        'event': event,
-    })
+        context['base_template'] = "layouts/base.html"
     else:
-        return render (request, "events/event_detail.html", {
-        'event': event,
-    })
+        context['base_template'] = "landing_page/base.html"
+    return render(request, "events/event_detail.html", context)
