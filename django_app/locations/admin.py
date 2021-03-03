9497860314930a3f82d0d1da0d72cbd3d5892707
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-03-03 20:39:49 +0200

    delete locations and make some changes in admin

--- django_app/locations/admin.py
+++ django_app/locations/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Location
-
-admin.site.register(Location)
-
--- django_app/locations/views.py
+++ django_app/locations/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 
-from .models import Location
 from authentication.models import Chapter
 
 
 def location_view(request):
     context = {}
     locations = Chapter.objects.all()
-    context['locations'] = locations
+    context["locations"] = locations
     if request.user.is_authenticated:
-        context['base_template'] = "layouts/base.html"
+        context["base_template"] = "layouts/base.html"
     else:
-        context['base_template'] = "landing_page/base.html"
+        context["base_template"] = "landing_page/base.html"
 
     return render(request, "locations/locations.html", context)
