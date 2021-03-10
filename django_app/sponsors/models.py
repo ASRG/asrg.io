4222fadd87f09633689414167b955292ec4e1222
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-03-10 15:25:39 +0200

    create plugin for sponsors

--- django_app/sponsors/models.py
+++ django_app/sponsors/models.py
@@ -2,12 +2,25 @@ from imagekit.models import ImageSpecField, ProcessedImageField
 from django.db import models
 
 
+STATUS_CHOICES = (
+    ("active", "Active"),
+    ("not-active", "Not active"),
+)
+
+
 class Sponsor(models.Model):
+    name = models.CharField(max_length=160, blank=False, primary_key=True)
+    website = models.URLField(blank=True, verbose_name="Sponsor website")
+    address = models.TextField(blank=True)
+    description = models.TextField(blank=False)
+    email = models.EmailField(verbose_name="General email address", blank=True)
+    start_date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[1][0]
+    )
     logo = ProcessedImageField(
         upload_to="sponsors/logos", blank=True, verbose_name="Company Logo"
     )
-    name = models.CharField(max_length=160, blank=False)
-    description = models.TextField(blank=False, verbose_name="Bio")
 
     def __str__(self):
         return self.name
