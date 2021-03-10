ffc6ac89b642e443fac9c9e4efa00b06827e735a
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-03-10 15:58:45 +0200

    add command to create test users

--- django_app/asrg/settings.py
+++ django_app/asrg/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "menus",  # helper for model independent hierarchical website navigation
     "sekizai",  # for javascript and css management
     # Internal Apps:
+    "asrg",
     "cms_app",
     "announcements",
     "job",
