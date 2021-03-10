44ac92bcc8d53857ce19d3f5135dca4a68e44a15
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-03-10 15:35:02 +0200

    use debug for gunicorn reload

--- django_app/gunicorn-cfg.py
+++ django_app/gunicorn-cfg.py
@@ -5,12 +5,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 import os
 
-RELOAD = os.environ.get("RELOAD_GUNICORN", False)
+RELOAD = os.environ.get("DEBUG", False)
 
-bind = '0.0.0.0:5005'
+bind = "0.0.0.0:5005"
 workers = 1
-accesslog = '-'
-loglevel = 'debug'
+accesslog = "-"
+loglevel = "debug"
 capture_output = True
 enable_stdio_inheritance = True
 if RELOAD:
