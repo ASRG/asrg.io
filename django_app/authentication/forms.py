4aae7a6e6de2024e2756e427edec5644a915fa52
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-03-14 14:31:43 +0200

    remove unused code

--- django_app/authentication/forms.py
+++ django_app/authentication/forms.py
@@ -7,8 +7,6 @@ Copyright (c) 2019 - present AppSeed.us
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import PasswordResetForm
-from django.contrib.auth.tokens import default_token_generator
 
 from .models import User, Chapter, OCCUPATIONAL_STATUS_CHOICES, GENDER_CHOICES
 from authentication.countries import COUNTRIES as COUNTRY_CHOICES
@@ -17,54 +15,6 @@ from .models import User, UserProfile
 DOB_CHOICES = []
 for i in range(1901, 2099):
     DOB_CHOICES.append(str(i))
-
-
-class CustomPasswordResetForm(PasswordResetForm):
-    """
-    Overriding the Email Password Resert Forms Save to be able to send HTML email
-    """
-
-    def save(
-        self,
-        domain_override=None,
-        email_template_name="passwordreset/password_reset_email.html",
-        use_https=False,
-        token_generator=default_token_generator,
-        request=None,
-        email_subject_name="passwordreset/password_reset_subject.txt",
-        **kwargs,
-    ):
-        from django.core.mail import EmailMultiAlternatives
-        from django.utils.html import strip_tags
-        from django.template.loader import render_to_string
-        from django.contrib.sites.shortcuts import get_current_site
-        from django.utils.http import int_to_base36
-
-        for user in self.users_cache:
-            if not domain_override:
-                current_site = get_current_site(request)
-                site_name = current_site.name
-                domain = current_site.domain
-            else:
-                site_name = domain = domain_override
-
-            c = {
-                "email": user.email,
-                "domain": domain,
-                "site_name": site_name,
-                "uid": int_to_base36(user.id),
-                "user": user,
-                "token": token_generator.make_token(user),
-                "protocol": use_https and "https" or "http",
-            }
-            render = render_to_string(email_template_name, c)
-            render_subject = render_to_string(email_subject_name, c)
-
-            msg = EmailMultiAlternatives(
-                render_subject, strip_tags(render), None, [user.email]
-            )
-            msg.attach_alternative(render, "text/html")
-            msg.send()
 
 
 class LoginForm(forms.Form):
