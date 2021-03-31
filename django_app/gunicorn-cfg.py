7b47b8997fb00dcb3bbd2bd12d1aa86ae9e9f831
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2021-03-31 20:44:27 +0300

    fix gunicorn reload

--- django_app/gunicorn-cfg.py
+++ django_app/gunicorn-cfg.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 import os
 
-RELOAD = os.environ.get("DEBUG", False)
+RELOAD = os.environ.get("DEBUG", "False")
 
 bind = "0.0.0.0:5005"
 workers = 1
@@ -13,6 +13,6 @@ accesslog = "-"
 loglevel = "debug"
 capture_output = True
 enable_stdio_inheritance = True
-if RELOAD:
+if RELOAD == "True":
     reload = True
     print("Reload for GUnicorn is active")
