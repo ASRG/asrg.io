3e0271af268c5889ea0fd8a29a58e67721e353a8
Melvin Thomas <melvinchooranolil@gmail.com>
2021-04-21 22:49:40 +0530

    data export ok

--- django_app/security_lab/views.py
+++ django_app/security_lab/views.py
@@ -2,32 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 
-# Create your views here.
-
-#import library
 import gspread
-#Service client credential from oauth2client
 from oauth2client.service_account import ServiceAccountCredentials
-# Print nicely
 import pprint
-#Create scope
 
 DIRNAME = os.path.dirname(__file__)
+# Create your views here.
+
 
 def security_lab(request):
+    context = {}
+    data = []
     scope = [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive',
     ]
-    #create some credential using that scope and content of startup_funding.json
     creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(DIRNAME, 'ASRG- Security-Lab-3ccc4fb856c3.json'),scope)
-    #create gspread authorize using that credential
     client = gspread.authorize(creds)
-    #Now will can access our google sheets we call client.open on StartupName
     sheet = client.open('Copy of Automotive Security Lab').sheet1
     pp = pprint.PrettyPrinter()
-    #Access all of the record inside that
     result = sheet.get_all_values()
-    # pp.pprint(result)
     result = list(result)
-    return HttpResponse(len(result))
+    context['theading'] = list(result[8])
+    for i in  range(9,len(result)):
+        data.append(list(result[i]))
+    context['data'] = data
+    # return HttpResponse(len(result))
+    return render(request, "security_lab.html", context)
